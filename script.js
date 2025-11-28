59ad14290e9c30f63bb46e209026e1e7aea2e441
masahiro <m.togasaki1026@gmail.com>
2025-11-28 14:44:23 +1100

    add node.js package and learned how to prompt(user inputs).

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log("Hello World !");
+// const v = 64
+
+// console.log("V=${v}");
+// console.log(`V=${v}`);
+// console.error("error");
+// console.warn("warning");
+
+// const prompt = require("prompt-sync")()
+
+// const v = prompt("type something")
+// console.log(v) 
+
+// variable type
+// var hello_world = "hello"
+// hello_world = "world "
+
+// // let is unmutable not changed later on
+// if(true){
+//     let x = 3
+// }
+// console.log(x)
+// // const is mutable  changed later on
+// const VELOCITY = 34
+
+// let and const are block scoped, which means it can't be accessed from 
+// the outside of blocks
+// On the other hand, var can be accessed because of function-scoped.
+
+// function tests(){
+//     console.log(x);
+//     var x = "test"
+//     console.log(x);
+// }
+
+// tests(); 
+// First console statement of x is undefined because before var x
+// Second console statement of x is printed because after var x
+
+
+// Arthimetic notation
+const x = 6
+const y = true
+console.log(x +y)
