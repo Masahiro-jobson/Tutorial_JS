65ce9b727604fd1f6f8c04671449bfa860f59065
masahiro <m.togasaki1026@gmail.com>
2025-11-29 14:03:00 +1100

    Learned difference between Array, Set, and Map

--- script.js
+++ script.js
@@ -98,10 +98,10 @@
 // console.log(arr2)
 // console.log(arr2[arr2.length -1])
 
-const arr = [1, 2, 3, true]
-const arr2 = Array.from("hello")
+// const arr = [1, 2, 3, true]
+// const arr2 = Array.from("hello")
 
-arr2.push(4)
+// arr2.push(4)
 // arr2.pop()
 // arr2.pop()
 // arr2.pop()
@@ -119,6 +119,105 @@ arr2.push(4)
 // const str = arr.join("|")
 // console.log(str);
 
-const arr3 = arr.splice(1,3)
-console.log(arr3)
-console.log(arr)
+// const arr3 = arr.splice(1,3)
+// console.log(arr3)
+// console.log(arr3)
+
+// Array Destruction
+// const [x, x2,...y] = [1,2,3,4]
+// console.log(x, x2, y) 
+// const x = [1,2,3,4]
+// const y = [1, 2, 3, ...x]
+// y.push("hello")
+// console.log(x, y) 
+
+// do while loop
+//  do{
+//     console.log("run")
+//     break
+// } while(true)
+    
+//  while(false){
+//     console.log("hello")
+//     break
+// }
+
+// For loop
+// const arr1 = [1,2,3]
+// for(let i=0; i< arr1.length; i++){
+//     console.log(arr[i])
+// }
+// for(let [i,value] of arr1.entries()){
+//     console.log(i,value)
+// }
+
+// Objects
+// => properties associated with value
+// JSON -> JavaScript Object Notation
+// const obj = {
+//     name:"Alice",
+//     Age:25,
+//     sayHello: function(){
+//         return "hello"
+//     }
+// }
+
+// obj.age = 26
+// obj.name = "Jobson"
+// obj["name"] = "Jack"
+// obj.newProp = [1,2]
+// delete obj.age
+
+// console.log(Object.keys(obj))
+// console.log(Object.values(obj))
+
+// for(let key in obj){
+//     console.log(key)
+// }
+
+// const obj2 = {
+//     hairColor: "gray",
+//     arr: [1,2,3,4],
+//     career:{}
+
+// } 
+
+// const obj3 = {...obj, ...obj2}
+// // obj3.career.info = "tech"
+// // obj2.arr.push("tech")
+// obj2.hairColor = "brown"
+
+// console.log(obj2, obj3)
+
+// Sets
+// const mySet = new Set([1,2,3]);
+// mySet.add(1)
+// mySet.delete(1)
+// mySet.has(1)
+// mySet.size
+
+// for(const value of mySet){
+
+// }
+
+// mySet.clear()
+// const arr = Array.from(mySet)
+// const arr2 = [...new Set(([1,2,3]))]
+// const arr3 = [...mySet]
+
+// MAPS
+// const myMap = new Map();
+// const numberMap = new Map([[1, "one"], [2, "two"]]);
+
+// myMap.set(4, "four")
+// myMap.delete(4)
+// myMap.get(1)
+// myMap.has(1)
+
+// for(const [key, value] of myMap){}
+
+// myMap.clear();
+
+// const arr = Array.from(myMap)
+
+// 
